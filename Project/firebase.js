643042160b10cddb08f73af77aacf0fa773f963e
Divyanshu Pandey <155071381+CodeCr4cker@users.noreply.github.com>
2025-05-31 22:07:52 +0530

    Create firebase.js

---
+++ Project/firebase.js
@@ -0,0 +1,21 @@
+// firebase.js
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFBtuIw0HVJl-HYZ9DSP1VZqwXMJli_W8",
+  authDomain: "darknet-chat-f6b5a.firebaseapp.com",
+  projectId: "darknet-chat-f6b5a",
+  storageBucket: "darknet-chat-f6b5a.appspot.com",
+  messagingSenderId: "485072993943",
+  appId: "1:485072993943:web:262edab82d07a87b4733d2",
+  measurementId: "G-2WL2PC8N6H",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
